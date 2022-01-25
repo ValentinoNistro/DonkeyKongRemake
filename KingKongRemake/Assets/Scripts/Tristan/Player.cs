6b8441086d94226717a170e5f1012e8346f5ac86
Tristan210381 <210381@student.glu.nl>
2022-01-25 13:57:15 +0100

    Victory screen scene

--- KingKongRemake/Assets/Scripts/Tristan/Player.cs
+++ KingKongRemake/Assets/Scripts/Tristan/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
@@ -43,6 +44,11 @@ public class Player : MonoBehaviour
         if (collision.gameObject.tag == "Ground")
         {
             cubeIsOnGround = true;
+        }
+
+        if (collision.gameObject.tag == "Princes")
+        {
+            SceneManager.LoadScene(1);
         }
     }
 }
